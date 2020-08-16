d8574ba8c613ac63961c4e7641d43215cd9c0e51
SharkMint <66984739+SharkMint@users.noreply.github.com>
2020-08-16 17:24:29 -0400

    Add files via upload

---
+++ Restaurante/js/efectos.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+	// Efecto Menu
+	$('.menu a').each(function(index, elemento){
+		$(this).css({
+			'top': '-200px'
+		});
+
+		$(this).animate({
+			top: '0'
+		}, 2000 + (index * 500));
+	});
+
+	// Efecto Header
+	if ( $(window).width() > 800 ){
+		$('Header .textos').css({
+			opacity: 0,
+			margintop: 0
+		});
+
+		$('Header .textos').animate({
+			opacity: 1,
+			margintop: '-52px'
+
+		}, 1500);
+	}
+
+	// Scroll elementos del menu
+	var acercaDe = $('#acerca-de').offset().top,
+		menu = $('#Platillos').offset().top,
+		galeria = $('#galeria').offset().top,
+		ubicacion = $('#ubicacion').offset().top;
+
+	$('#btn-acerca-de').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: acercaDe
+		}, 500);
+	});
+
+	$('#btn-menu').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: menu
+		}, 500);
+	});
+
+	$('#btn-galeria').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: galeria
+		}, 500);
+	});
+
+	$('#btn-ubicacion').on('click', function(e){
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: ubicacion
+		}, 500);
+	});
+
+});
